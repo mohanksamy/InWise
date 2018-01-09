eec1e1751d7ac5a17a380c4bbf8de373d5d86acd
Mohan Kandasamy <mohanksamy@gmail.com>
2018-01-09 08:17:20 -0800

    Added DTO classes

---
+++ INWISE-COMMON/src/main/java/com/prod/inwise/dto/BaseDTO.java
@@ -0,0 +1,90 @@
+package com.prod.inwise.dto;
+
+import java.sql.Timestamp;
+
+/**
+ * @author mohan_kandasamy
+ *
+ */
+public abstract class BaseDTO {
+
+	private boolean active;
+	
+	private String createdUser;
+
+	private Timestamp createdTS;
+	
+	private String modifiedUser;
+	
+	private Timestamp modifiedTS;
+
+	/**
+	 * @return the active
+	 */
+	public boolean isActive() {
+		return active;
+	}
+
+	/**
+	 * @param active the active to set
+	 */
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	/**
+	 * @return the createdUser
+	 */
+	public String getCreatedUser() {
+		return createdUser;
+	}
+
+	/**
+	 * @param createdUser the createdUser to set
+	 */
+	public void setCreatedUser(String createdUser) {
+		this.createdUser = createdUser;
+	}
+
+	/**
+	 * @return the createdTS
+	 */
+	public Timestamp getCreatedTS() {
+		return createdTS;
+	}
+
+	/**
+	 * @param createdTS the createdTS to set
+	 */
+	public void setCreatedTS(Timestamp createdTS) {
+		this.createdTS = createdTS;
+	}
+
+	/**
+	 * @return the modifiedUser
+	 */
+	public String getModifiedUser() {
+		return modifiedUser;
+	}
+
+	/**
+	 * @param modifiedUser the modifiedUser to set
+	 */
+	public void setModifiedUser(String modifiedUser) {
+		this.modifiedUser = modifiedUser;
+	}
+
+	/**
+	 * @return the modifiedTS
+	 */
+	public Timestamp getModifiedTS() {
+		return modifiedTS;
+	}
+
+	/**
+	 * @param modifiedTS the modifiedTS to set
+	 */
+	public void setModifiedTS(Timestamp modifiedTS) {
+		this.modifiedTS = modifiedTS;
+	}
+}
---
+++ INWISE-COMMON/src/main/java/com/prod/inwise/dto/StoreDTO.java
@@ -0,0 +1,90 @@
+package com.prod.inwise.dto;
+
+import java.math.BigInteger;
+
+/**
+ * @author mohan_kandasamy
+ *
+ */
+public class StoreDTO extends BaseDTO {
+	
+	private Long id;
+	
+	private String name;
+	
+	private String address;
+	
+	private String uin;
+	
+	private BigInteger phone;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * @return the uin
+	 */
+	public String getUin() {
+		return uin;
+	}
+
+	/**
+	 * @param uin the uin to set
+	 */
+	public void setUin(String uin) {
+		this.uin = uin;
+	}
+
+	/**
+	 * @return the phone
+	 */
+	public BigInteger getPhone() {
+		return phone;
+	}
+
+	/**
+	 * @param phone the phone to set
+	 */
+	public void setPhone(BigInteger phone) {
+		this.phone = phone;
+	}
+}
