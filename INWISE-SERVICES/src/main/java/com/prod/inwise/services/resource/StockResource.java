3577a757b33b4c4aa758716a7775c49e387f0df4
loguvsr <loguvsr@gmail.com>
2018-01-25 22:19:28 +0530

    Added Resource files

---
+++ INWISE-SERVICES/src/main/java/com/prod/inwise/services/resource/StockResource.java
@@ -0,0 +1,53 @@
+package com.prod.inwise.services.resource;
+
+import static javax.ws.rs.core.Response.status;
+import static javax.ws.rs.core.Response.Status.OK;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.prod.inwise.services.data.Stock;
+import com.prod.inwise.services.repo.StockRepository;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+/**
+ * REST Resource exposes operation on Stock resource
+ * 
+ * @author Logu
+ *
+ */
+@Component
+@Path("/stock")
+@Produces(MediaType.APPLICATION_JSON)
+@Api(value = "Stock Service")
+public class StockResource {
+
+	@Autowired
+	private StockRepository stockRepo;
+
+	@POST
+	@ApiOperation(value = "Create stock", notes = "Create stock model")
+	@ApiResponses(value = {
+			@ApiResponse(code = 404, message = "Invalid tenant specified"),
+			@ApiResponse(code = 401, message = "Invalid user specified"),
+			@ApiResponse(code = 401, message = "No permission to access model"),
+			@ApiResponse(code = 401, message = "No privilege to access model"),
+			@ApiResponse(code = 440, message = "invalid session or access-token specified"),
+			@ApiResponse(code = 500, message = "Server Internal error")})
+	public Response createStock(Stock stock) {
+
+		stockRepo.save(stock);
+
+		return status(OK).build();
+	}
+}
