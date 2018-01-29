b634a28934cd917f5a7717a71d0318c5959f4a68
loguvsr <loguvsr@gmail.com>
2018-01-29 21:36:21 +0530

    Added Constant values

--- INWISE-SERVICES-TEST/src/test/java/com/prod/inwise/services/test/util/Constants.java
+++ INWISE-SERVICES-TEST/src/test/java/com/prod/inwise/services/test/util/Constants.java
@@ -7,39 +7,36 @@ import com.google.gson.Gson;
  *
  */
 public interface Constants {
-	
-	Gson gson 					= new Gson();
 
-	String BASE_URI				= "http://localhost";
-	
-	int PORT						= 8080;
-	
-	String BASE_PATH				= "inwise";
-	
-	String URL_DELIMITER			= "/";
-	
+	Gson gson = new Gson();
+
+	String BASE_URI = "http://localhost";
+
+	int PORT = 8080;
+
+	String BASE_PATH = "inwise";
+
+	String URL_DELIMITER = "/";
+
 	// Resources
-	String RESOURCE_PATH_HEALTH	= "health";
-	
-	String RESOURCE_PATH_STORE 	= "store";
-	
-	String RESOURCE_PATH_TAX 	= "tax";
-	
-	String RESOURCE_PATH_ITEM 	= "item";
-	
-	/*String RESOURCE_HEALTH 	= "health";
-	
-	String RESOURCE_HEALTH 	= "health";
-	
-	String RESOURCE_HEALTH 	= "health";
-	
-	String RESOURCE_HEALTH 	= "health";
-	
-	String RESOURCE_HEALTH 	= "health";
-	
-	String RESOURCE_HEALTH 	= "health";
-	
-	String RESOURCE_HEALTH 	= "health";
-	
-	String RESOURCE_HEALTH 	= "health";*/
+	String RESOURCE_PATH_HEALTH = "health";
+
+	String RESOURCE_PATH_STORE = "store";
+
+	String RESOURCE_PATH_TAX = "tax";
+
+	String RESOURCE_PATH_ITEM = "item";
+
+	String RESOURCE_PATH_INVOICE = "invoice";
+
+	String RESOURCE_PATH_LINEITEM = "lineItem";
+
+	String RESOURCE_PATH_STOCK = "stock";
+
+	String RESOURCE_PATH_STOCK_BATCH = "stockBatch";
+
+	String RESOURCE_PATH_STOCK_HISTORY = "stockHistory";
+
+	String RESOURCE_PATH_VENDOR = "vendor";
+
 }
